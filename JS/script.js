070a03faa8a37adda5ac35e158910ceb397b1b04
Ananias <amychelleoropezaortiz@gmail.com>
2023-12-01 21:27:27 -0500

    Terminada la logica

--- JS/script.js
+++ JS/script.js
@@ -1,55 +1,55 @@
-let palabras = ["programacion", "riwi", "ingles", "python", "javascript"];
-let palabraSecreta = palabras[Math.floor(Math.random()*palabras.length)];
-let palabraMostrada = '_ '.repeat (palabraSecreta.length);
-let intentosRestantes = 6;
+const palabras = ["programador", "riwi", "ingles", "python", "javascript"];
+let palabraSecreta = palabras[Math.floor(Math.random() * palabras.length)];
+let palabraMostrada = '_ '.repeat(palabraSecreta.length);
+let intentosRestantes = 7;
 let letrasUsadas = [];
 
-function mostrarPalabra () {
+function mostrarPalabra() {
     document.getElementById("palabra").textContent = palabraMostrada;
     document.getElementById("intentos").textContent = intentosRestantes;
     document.getElementById("letrasUsadas").textContent = letrasUsadas.join(', ');
 }
 
-function adivinar () {
-    let letra = document.getElementById("adivinarInput").ariaValueMax.toLowerCase();
+function adivinar() {
+    let letra = document.getElementById("adivinarInput").value.toLowerCase();
 
     if (letrasUsadas.includes(letra)) {
-        alert("Ya has usado esta letra.");
+        alert('Ya has usado esta letra.');
         return;
-    };
+    }
 
     letrasUsadas.push(letra);
     let acierto = false;
 
-    for (let seguidor = 0; seguidor < palabraSecreta.length; seguidor++) {
-        if (palabraSecreta[seguidor] === letra) {
+    for (let i = 0; i < palabraSecreta.length; i++) {
+        if (palabraSecreta[i] === letra) {
             acierto = true;
-            palabraMostrada = palabraMostrada.substring(0, seguidor) + letra + palabraMostrada.substring(seguidor + 1);
-        };
-    };
+            palabraMostrada = palabraMostrada.substring(0, i) + letra + palabraMostrada.substring(i + 1);
+        }
+    }
 
     if (!acierto) {
         intentosRestantes--;
-    };
+    }
 
     if (intentosRestantes === 0) {
-        alert("¡Oh no!, has perdido, la palabra era: " + palabraSecreta);
+        alert('¡Oh no!, has perdido. La palabra era: ' + palabraSecreta);
         reiniciarJuego();
     } else if (palabraMostrada === palabraSecreta) {
-        alert("¡Muy bien!, has ganado el juego, date a ti mismo una estrellita.");
+        alert('¡Muy bien!, has adivinado la palabra, date una estrella a ti mismo.');
         reiniciarJuego();
-    };
+    }
 
     mostrarPalabra();
-        document.getElementById("adivinarInput").value = "";
-    
-    function reiniciarJuego() {
-        palabraSecreta = palabras[Math.floor(Math.random()*palabras.length)];
-        palabraMostrada = '_ '.repeat (palabraSecreta.length);
-        intentosRestantes = 6;
-        letrasUsadas = [];
-        mostrarPalabra();
-    };
+    document.getElementById("adivinarInput").value = '';
+}
 
+function reiniciarJuego() {
+    palabraSecreta = palabras[Math.floor(Math.random() * palabras.length)];
+    palabraMostrada = '_'.repeat(palabraSecreta.length);
+    intentosRestantes = 6;
+    letrasUsadas = [];
     mostrarPalabra();
 }
+
+mostrarPalabra();
